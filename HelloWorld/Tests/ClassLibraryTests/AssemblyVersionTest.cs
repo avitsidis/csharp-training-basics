7b839ece7726699f027d490b917c60e46d773dfc
avit sidis <avit.sidis@gmail.com>
2017-08-28 00:54:21 +0200

    add assembly version test

---
+++ HelloWorld/Tests/ClassLibraryTests/AssemblyVersionTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace HelloWorld.Tests.ClassLibraryTests
+{
+    public class AssemblyVersionTest
+    {
+        [Fact]
+        public void HelloWorldModel_version_is_2_0_0_0()
+        {
+            var expectedVersion = "2.0.0.0";
+            var version = HelloWorldModelContext.ModelAssembly.GetName().Version.ToString();
+            Assert.Equal(expectedVersion, version);
+        }
+    }
+}
