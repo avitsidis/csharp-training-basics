d1736f705cd0edbda6729ded3fe6cb57559f912d
avit sidis <avit.sidis@gmail.com>
2017-08-27 23:46:37 +0200

    refactor ClassLibraryTests because some code will be reused by other tests

---
+++ HelloWorld/Tests/ClassLibraryTests/AssemblyReferenceAndPropertiesTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace HelloWorld.Tests.ClassLibraryTests
+{
+    public class AssemblyReferenceAndPropertiessTest
+    {
+        [Fact]
+        public void HelloWorld_references_HelloWorld_Model()
+        {
+            var referencedAssemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(a => a.Name);
+            Assert.Contains(HelloWorldModelContext.AssemblyName, referencedAssemblies);
+        }
+
+        [Fact]
+        public void HelloWorld_Model_defines_a_Person_class()
+        {
+            Assert.Contains(HelloWorldModelContext.ModelAssembly.DefinedTypes, t => t.FullName == "HelloWorld.Model.Person");
+        }
+
+
+        [Fact]
+        public void Person_class_defines_LastName_property()
+        {
+            TypeAssert.TypeHasAPropertyNamed(HelloWorldModelContext.PersonType, "LastName");
+        }
+
+        [Fact]
+        public void Person_class_defines_FirstName_property()
+        {
+            TypeAssert.TypeHasAPropertyNamed(HelloWorldModelContext.PersonType, "FirstName");
+        }
+
+        [Fact]
+        public void Person_class_defines_NISS_property()
+        {
+            TypeAssert.TypeHasAPropertyNamed(HelloWorldModelContext.PersonType, "NISS");
+        }
+
+        [Fact]
+        public void Person_NISS_throws_ArgumentException_When_non_digit_characters_are_provided()
+        {
+            dynamic person = Activator.CreateInstance(HelloWorldModelContext.PersonType);
+            Assert.Throws<ArgumentException>(() => person.NISS = "a");
+        }
+
+        [Fact]
+        public void Person_NISS_is_changed_when_digit_characters_are_provided()
+        {
+            string validNISS = "86031212312";
+            dynamic person = Activator.CreateInstance(HelloWorldModelContext.PersonType);
+            person.NISS = validNISS;
+            Assert.Equal(validNISS, person.NISS);
+        }
+    }
+}
---
+++ HelloWorld/Tests/ClassLibraryTests/HelloWorldModelContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace HelloWorld.Tests.ClassLibraryTests
+{
+    public static class HelloWorldModelContext
+    {
+        public static string AssemblyName => "HelloWorld.Model";
+        public static Assembly ModelAssembly => Assembly.Load(Assembly.GetExecutingAssembly()
+                   .GetReferencedAssemblies()
+                   .Single(a => a.Name == AssemblyName));
+
+        public static Type PersonType
+        {
+            get
+            {
+                var personTypeFullName = $"{AssemblyName}.Person";
+                return Assembly.Load(Assembly.GetExecutingAssembly()
+                   .GetReferencedAssemblies()
+                   .Single(a => a.Name == AssemblyName))
+                   .DefinedTypes.Single(t => t.FullName == personTypeFullName);
+            }
+        }
+    }
+}
