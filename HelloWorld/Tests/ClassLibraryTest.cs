f52bb560de3a4405555a4a279af6e3d26335c47b
avit sidis <avit.sidis@gmail.com>
2017-08-27 23:25:04 +0200

    add NISS tests for Assembly reference & properties exercice

---
+++ HelloWorld/Tests/ClassLibraryTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace HelloWorld.Tests
+{
+    public class ClassLibraryTest
+    {
+        public class AssemblyReferenceAndPropertiessTest
+        {
+            const string MODEL_ASSEMBLY_NAME = "HelloWorld.Model";
+            private static Assembly ModelAssembly => Assembly.Load(Assembly.GetExecutingAssembly()
+                       .GetReferencedAssemblies()
+                       .Single(a => a.Name == MODEL_ASSEMBLY_NAME));
+
+            private Type PersonType {
+            get {
+                    return Assembly.Load(Assembly.GetExecutingAssembly()
+                       .GetReferencedAssemblies()
+                       .Single(a => a.Name == MODEL_ASSEMBLY_NAME))
+                       .DefinedTypes.Single(t => t.FullName == "HelloWorld.Model.Person");
+                }
+            }
+            
+
+            [Fact]
+            public void HelloWorld_references_HelloWorld_Model()
+            {
+                var referencedAssemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(a => a.Name);
+                Assert.Contains("HelloWorld.Model", referencedAssemblies);
+            }
+
+            [Fact]
+            public void HelloWorld_Model_defines_a_Person_class()
+            {
+                Assert.Contains(ModelAssembly.DefinedTypes, t => t.FullName == "HelloWorld.Model.Person");
+            }
+
+            [Fact]
+            public void Person_class_defines_properties()
+            {
+                const string lastName = "LastName";
+                const string firstName = "FirstName";
+                const string niss = "NISS";
+                Assert.Contains(PersonType.GetProperties(), p => p.Name == lastName);
+                Assert.Contains(PersonType.GetProperties(), p => p.Name == firstName);
+                Assert.Contains(PersonType.GetProperties(), p => p.Name == niss);
+            }
+
+
+            [Fact]
+            public void Person_NISS_throws_ArgumentException_When_non_digit_characters_are_provided()
+            {
+                dynamic person = Activator.CreateInstance(PersonType);
+                Assert.Throws<ArgumentException>(() => person.NISS = "a");
+            }
+
+            [Fact]
+            public void Person_NISS_is_changed_when_digit_characters_are_provided()
+            {
+                string validNISS = "86031212312";
+                dynamic person = Activator.CreateInstance(PersonType);
+                person.NISS = validNISS;
+                Assert.Equal(validNISS, person.NISS);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,20 +8,27 @@
 - Access Configuration content with System.Configuration.ConfigurationManager.AppSettings and print myparameter value in console output 
 
 ### Class Library
-- create new assembly named Model
+#### Assembly reference & properties
+- create new assembly named HelloWorld.Model
 - create a class Person 
-- add a string property LastName in Person
-- add a string property FirstName in Person
-- add a string property NISS in Person (NISS can only contain digits)
-- add reference in HelloWorld to Model
+    - add a string property LastName in Person
+    - add a string property FirstName in Person
+    - add a string property NISS in Person 
+		- NISS can only contain digits
+        - throw ArgumentException Exception if format is not correct
+- add reference in HelloWorld to HelloWorld.Model
 - in HelloWorld assembly, instanciate some Person objects and print them in the console
-- override ToString() method in class Person (returns "FirstName LastName(NISS)")
+#### Computed property
 - add a DateTime property Birthdate in Person
 - add a Computed int property Age in Person (get only, no set)
+#### inheritance & polymorphism
+- override ToString() method in class Person (returns "FirstName LastName(NISS)")
 - create a class Consultant that inherits from Person
 - add a Date property HireDate
 - reimplement Birthdate to enforce that consultants must be older than 18 years old
+    - this property must be polymorphic
 - override ToString() method in class Person (returns "FirstName LastName(NISS) # years of experience")
+#### Assembly version
 - change assembly version of Model to 2.0.0.0
 
 ### Nuget packages
